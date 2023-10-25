32cfe7aee3df19ac18bc12264a87c39540416c28
Amar Badnjevic <badnja25@gmail.com>
2023-10-25 13:17:05 +0200

    change is_active to default false and create migrations

--- src/core/models.py
+++ src/core/models.py
@@ -25,7 +25,7 @@ class User(Base):
     last_name = Column(String(150), nullable=False)
     email = Column(String(120), unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True, nullable=False)
+    is_active = Column(Boolean, default=False, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     user_details = relationship("UserDetail",
